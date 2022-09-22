b8e1a75e793be097c9468f149e662b0b805a0d1c
sebastianmb <sebastianmb88@gmail.com>
2022-09-21 23:43:42 -0500

    Logica reconocer llave entre df

--- logistica/index.py
+++ logistica/index.py
@@ -1,9 +1,18 @@
 import pandas as pd
+import numpy as np
 import xlrd
 
 path="C:\\ProyectosWeb\\assigment\\logistica\\"
 dfcargue= pd.read_excel(path+'cargue.xlsx')
 dfassignment=pd.read_excel(path+'asignacion.xlsx')
 
+print(dfcargue)
+#cprint(list(dfcargue))
 
-print(list(dfassignment))
+#dfassignment['check']=dfcargue.Codigo.isin(dfcargue.Codigo)
+#print(dfassignment['check'].value_counts())
+#print(dfassignment.loc[dfassignment.check== True, 'Codigo'])
+
+
+#dfassignment['CodigoCargue']=np.where(dfassignment['Codigo']==3,1,0)
+
