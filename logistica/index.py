6ca7f863a852ab1543d6a621340c4a2f4cfc52fd
sebastianmb <sebastianmb88@gmail.com>
2022-09-22 23:21:13 -0500

    Asignación de nivel al cargue por jupyter y pandas

--- logistica/index.py
+++ logistica/index.py
@@ -1,18 +1,23 @@
+# Importar librerias
 import pandas as pd
 import numpy as np
 import xlrd
-
+#Leer dataframe
 path="C:\\ProyectosWeb\\assigment\\logistica\\"
 dfcargue= pd.read_excel(path+'cargue.xlsx')
 dfassignment=pd.read_excel(path+'asignacion.xlsx')
-
-print(dfcargue)
-#cprint(list(dfcargue))
-
-#dfassignment['check']=dfcargue.Codigo.isin(dfcargue.Codigo)
-#print(dfassignment['check'].value_counts())
-#print(dfassignment.loc[dfassignment.check== True, 'Codigo'])
-
-
-#dfassignment['CodigoCargue']=np.where(dfassignment['Codigo']==3,1,0)
+​
+#Merge de los dos dataframes
+merged_inner=pd.merge(left=dfcargue, right=dfassignment, left_on='Codigo', right_on='Codigo')
+​
+#merged_inner
+#Export Dataframe to excel
+prueba=pd.DataFrame(merged_inner)
+​
+filename='prueba.xlsx'
+​
+prueba.to_excel(filename)
+print('prueba record successfully exported into Excel File')
+​
+prueba record successfully exported into Excel File
 
