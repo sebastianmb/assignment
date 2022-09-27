7a9b8b521e7a9d6489d3340dbea1870ce04c2abf
sebastianmb <sebastianmb88@gmail.com>
2022-09-26 23:38:35 -0500

    Se unifican tablas eliminando Unnamed

--- logistica/index.py
+++ logistica/index.py
@@ -1,4 +1,4 @@
-# Importar librerias
+tar librerias
 import pandas as pd
 import numpy as np
 import xlrd
@@ -7,22 +7,19 @@ path="C:\\ProyectosWeb\\assigment\\logistica\\"
 dfcargue= pd.read_excel(path+'cargue.xlsx')
 dfassignment=pd.read_excel(path+'asignacion.xlsx')
 ​
-#
 ​
 # Get names of indexes for which column Stock has value No
-indexNames = dfcargue[ dfcargue['Codigo'] == 'A'].index
-# Delete these row indexes from dataFrame
-dfcargue.drop(indexNames , inplace=True)
+​
 # Convertir el campo record_id de integer a float
-#dfcargue['Codigo'] = dfcargue['Codigo'].astype('int64')
-#dfcargue['Codigo'].dtype
-dfcargue = dfcargue.reset_index(drop=True)
+dfcargue['Codigo'] = dfcargue['Codigo'].astype('int64')
+dfcargue['Codigo'].dtype
+​
 #print(dfcargue)
+dtype('int64')
 #Merge de los dos dataframes
 merged_inner=pd.merge(left=dfcargue, right=dfassignment, left_on='Codigo', right_on='Codigo')
 ​
 #merged_inner
-#
 #Export Dataframe to excel
 prueba=pd.DataFrame(merged_inner)
 ​
@@ -32,5 +29,15 @@ prueba.to_excel(filename)
 ​
 ​
 print('prueba record successfully exported into Excel File')
-#prueba
+​
+prueba record successfully exported into Excel File
+df = prueba.loc[:, ~prueba.columns.str.contains('^Unnamed')]
+​
+filename='df.xlsx'
+​
+prueba.to_excel(filename)
+​
+​
+print('prueba record successfully exported into Excel File')
+​
 prueba record successfully exported into Excel File
