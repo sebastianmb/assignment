0aecb9d8baf984a24c271a4c156730b627e9fc97
sebastianmb <sebastianmb88@gmail.com>
2022-10-16 21:14:08 -0500

    Base de datos sin datos na ni texto

--- logistica/index.py
+++ logistica/index.py
@@ -1,98 +1,20 @@
- Importar librerias
+ # Importar librerias
 import pandas as pd
 import numpy as np
 import xlrd
+
 #Leer dataframe
 path="C:\\ProyectosWeb\\assigment\\logistica\\"
 dfcargue= pd.read_excel(path+'cargue.xlsx')
 dfassignment=pd.read_excel(path+'asignacion.xlsx')
-​
-​
+
 #llenar campo nan con 0
-df_filled_nan=dfcargue.fillna(0)
-#llenar campo texto por 0
+dfcargue_fill=dfcargue.fillna(0)
 
-# Get names of indexes for which column Stock has value No
-​
+#Eliminar campos con texto
+marcas=['COLOMBINA','ALDOR','ALIMENTOS CONCENTRADOS RAZA','AZULK SA','COLOMBIANA DE COMERCIO CORBETA','COMESTIBLES ITALO SA','HARINERA DEL VALLES.A.','HENKEL COLOMBIANA SAS','INDUSTRIAS KATORI','J&J DE COLOMBIA SA','KELLOGG DE COLOMBIA','LABORATORIOS NATURAL FRESHLY','PAPELES NACIONALES SA','PFIZER CONSUMER HEALTCHARE','UNILEVER','VELAS Y VELONES SAN JORGE','INVERSIONES VADISA SOCIEDAD POR ACCIONES SIMPLIFICADA']
+dfcargue_ok=dfcargue_fill.replace(marcas,0)
+
 # Convertir el campo record_id de integer a float
-dfcargue['Codigo'] = dfcargue['Codigo'].astype('int64')
-dfcargue['Codigo'].dtype
-​
-#print(dfcargue)
-dtype('int64')
-#Merge de los dos dataframes
-merged_inner=pd.merge(left=dfcargue, right=dfassignment, left_on='Codigo', right_on='Codigo')
-​
-#merged_inner
-#
-#Export Dataframe to excel
-prueba=pd.DataFrame(merged_inner)
-​
-filename='prueba.xlsx'
-​
-#prueba.to_excel(filename)
-​
-​
-#print('prueba record successfully exported into Excel File')
-#prueba
-df = prueba.loc[:, ~prueba.columns.str.contains('^Unnamed')]
-​
-filename='df.xlsx'
-​
-#prueba.to_excel(filename)
-​
-​
-#print('prueba record successfully exported into Excel File')
-#df.head()
-#filtrado por niveles
-nivel11 = df['Nivel'] == 2.0
-nivel11
-df_nivel11 = df[nivel11]
-#df_nivel11.head()
-#filtrado por niveles
-nivel2 = df['Nivel'] == 2.0
-nivel2
-df_nivel2 = pd.DataFrame(df[nivel2])
-
-filename='df_nivel2.xlsx'
-
-df_nivel2.to_excel(filename)
-
-df_nivel2.head()
-TotalV2u = df_nivel2['Venta'].sum()
-
-TotalV2p = df_nivel2['Precio'].sum()
-
-
-print ("Unidades Totales nivel 2: "+TotalV2u.astype(str))
-print ("Precio Total nivel 2: "+TotalV2p.astype(str))
-
-#Cantidad de filas y columnas en el filtro nivel 2
-
-index=pd.Series(df_nivel2.shape)
-
-print(index)
-
-personas= input("Ingresa la cantidad de personas que van a cargar en el nivel 2: ")
-split=int(index[0]/int(personas))
-
-# Dividir el numero de filas en partes iguales
-
-#Dividir Dataframe en partes iguales ejemplo en dos partes iguales
-
-df_nivel2_1 = df_nivel2.iloc[:split,:]
-
-
-df_nivel2_2=df_nivel2.iloc[split:,:]
-
-filename='nivel2_1.xlsx'
-
-df_nivel2_1.to_excel(filename)
-
-df_nivel2_1.head()
-
-df_nivel2_2.head()
-filename='nivel2_2.xlsx'
-
-df_nivel2_2.to_excel(filename)
-
+dfcargue_ok['Codigo'] = dfcargue_ok['Codigo'].astype('int64')
+dfcargue_ok['Codigo'].dtype
