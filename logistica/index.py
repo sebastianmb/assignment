9243d14adc0edd82b5ab6170eb3f930e7578a576
sebastianmb <sebastianmb88@gmail.com>
2022-10-18 22:05:37 -0500

    Dataframe que compila los subtotales por rutas ciclo for condicional if

--- logistica/index.py
+++ logistica/index.py
@@ -34,15 +34,31 @@ df_ruta.to_excel(filename)
 df_ruta.head()
 
 #filtrado por niveles
+ruta=1
 Niveles =[1.1,1.2,1.3,2.0,3.0]
 filtro = df_ruta['Nivel'] == 1.1
 
+df = pd.DataFrame(columns=['Ruta','Nivel','Unidades', 'Precio'])
 
 for i in Niveles:
     filtro = df_ruta['Nivel'] == i
     filtro
     name= str(i)
     df_nivel = pd.DataFrame(df_ruta[filtro])
+    
+    totalU = df_nivel['Venta'].sum()
+    totalP = df_nivel['Precio'].sum()
+    
+    df_new_row = pd.DataFrame({ 'Ruta': [ruta], 'Nivel': [name], 'Unidades':[totalU], 'Precio':[totalP] })
+      
+    df=pd.concat([df, df_new_row])
+    if name=='3.0':
+        ruta= ruta+1
+    
+     
+    print ("Unidades Totales nivel "+name+": "+totalU.astype(str))
+    print ("Precio Total nivel "+name+": "+totalP.astype(str))
+    print ("****************************************************************\n")
     
     filename='df_nivel+'+name+'.xlsx'
 
